6c505c8aa77d10f1da9c2bb6063cba66d501bd01
Tanya Margulis <Tany999m@gmail.com>
2020-08-15 14:49:30 +0200

    Updates

---
+++ precache-manifest.67a6206edf5c5880b519a61c9edfad08.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dad7c5cf3b14d7def19b037e8b6a004",
+    "url": "/around-react/index.html"
+  },
+  {
+    "revision": "75571c66d163c7afa0d5",
+    "url": "/around-react/static/css/main.765604c4.chunk.css"
+  },
+  {
+    "revision": "ead50d2402eac01070a8",
+    "url": "/around-react/static/js/2.baec95bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/around-react/static/js/2.baec95bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75571c66d163c7afa0d5",
+    "url": "/around-react/static/js/main.f14a0bff.chunk.js"
+  },
+  {
+    "revision": "26160d15b3a5de0a48a5",
+    "url": "/around-react/static/js/runtime-main.a5c5d8c7.js"
+  },
+  {
+    "revision": "001893789f7f342b520f29ac8af7d6ca",
+    "url": "/around-react/static/media/Inter-Bold.00189378.woff"
+  },
+  {
+    "revision": "b82eef497995d28fffc2ef185211e282",
+    "url": "/around-react/static/media/Inter-ExtraBold.b82eef49.woff"
+  },
+  {
+    "revision": "3ae6a7d3890c33d857fc00bd2e4c4820",
+    "url": "/around-react/static/media/Inter-Regular.3ae6a7d3.woff"
+  },
+  {
+    "revision": "bdfb8ebb113b9312e080c2f4b4213223",
+    "url": "/around-react/static/media/add.bdfb8ebb.svg"
+  },
+  {
+    "revision": "9fdebb6b009c16af50fae58657e30a94",
+    "url": "/around-react/static/media/black-heart.9fdebb6b.svg"
+  },
+  {
+    "revision": "c2d0901f14de58acae456bda23f80e8a",
+    "url": "/around-react/static/media/close-icon.c2d0901f.svg"
+  },
+  {
+    "revision": "58afbe73d6547d1730a443f381c68d9e",
+    "url": "/around-react/static/media/edit.58afbe73.svg"
+  },
+  {
+    "revision": "6c74d9b53442a247e20750f5de0be939",
+    "url": "/around-react/static/media/heart.6c74d9b5.svg"
+  },
+  {
+    "revision": "3e6893cd2eab107d2d5385c921e95382",
+    "url": "/around-react/static/media/logo.3e6893cd.svg"
+  },
+  {
+    "revision": "9c3b03ae735a14dc757b4d25c0ba5fb3",
+    "url": "/around-react/static/media/trash.9c3b03ae.svg"
+  }
+]);
